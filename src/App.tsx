5f563be7b671ad69141d9676be680cf9b0dfff83
Dean <deaneffi1414@gmail.com>
2025-04-16 14:04:29 +0300

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-cstm-purple p-4 text-6xl">
+        This will be my portfolio! lorem
+      </h1>
+    </>
+  );
+}
+
+export default App;
